6dde79c4e4a871c0ffab9ce9d5ed90787ca3b521
Patrick <sanpatricio@users.noreply.github.com>
2019-05-28 17:49:59 +0200

    Modified output format and gave more data.  Error handling
    Will now return a link to the NIST entry for CVEs that are found affecting a given OS.  More error handling.

--- CeVichE.py
+++ CeVichE.py
@@ -17,6 +17,11 @@
 import bs4 as bs
 import urllib2, sys, lxml, re
 
+def buildLink(cve):
+        nistURL = "https://nvd.nist.gov/vuln/detail/"
+        link = nistURL + cve
+        return link
+
 baseURL  = 'https://access.redhat.com/security/cve/'
 filename = "CVEs.txt"
 
@@ -28,30 +33,42 @@ with open(filename) as inputfile:
                 # Red Hat's website does not appreciate being scraped by
                 # automatons.  *Poof!*  We are now a Mozilla browser.
                 agentSpoof = {'User-Agent': 'Mozilla/5.0'}
-
                 sauce = urllib2.Request(fullURL,headers=agentSpoof)
+                
+                # Grabbing the page and dealing with any errors that may occur.
                 try:
+                        # Make contact with the Red Hat page
                         page  = urllib2.urlopen(sauce)
                 except urllib2.HTTPError, e:
+                        # An HTTP error has occurred, likely a 404 indicating
+                        # the CVE doesn't exist on Red Hat's website
                         status_code = e.code
                         print ("%s: An HTTP error has occured - %s" % (cve, status_code))
                 except urllib2.URLError, e:
+                        # Something is wrong with the URL
                         status_code = e.code
                         print ("%s: A URL error has occured - %s" % (cve, status_code))
                 except (TypeError, AttributeError, KeyError) as e:
+                        # Something happened that I didn't anticipate
                         status_code = e.code
                         print ("%s: An unspecified error has occured - %s" % (cve, status_code))
                 else:
+                        # Success
                         status_code = page.code
                         soup  = bs.BeautifulSoup(page,'lxml')
-                
-                        page  = urllib2.urlopen(sauce)
-                        soup  = bs.BeautifulSoup(page,'lxml')
-
                         theRows = soup.find_all('tr')
+
                         for row in theRows:
-                                technology = row.find('th', text=re.compile(r"Red Hat Enterprise Linux [67]"))
-                                if technology:
-                                        os    = technology.string
+                                # The following regex seeks only rows labeled "Red Hat Enterprise Linux 6" or "7".
+                                # An end-of-line of $ was used to prevent matching on RHEL "7.3 Telco Extended Update..."
+                                tech = row.find('th', text=re.compile(r"Red Hat Enterprise Linux [67]$"))
+
+                                if 'None' in str(tech):
+                                        continue
+                                else:
                                         state = row.find('td', attrs={'headers':'th-state'}).string
-                                        print ("%s: %s is %s" % (cve, os, state))
+                                        package = row.find('td', attrs={'headers':'th-package'}).string
+                                        link = ""
+                                        if state == "Affected":
+                                                link = "- " + buildLink(cve)
+                                        print ("[%s] %s %s package: %s %s" % (cve, tech.string, package, state, link))
