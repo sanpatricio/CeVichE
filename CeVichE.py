6b1cf9974da89e371a22ffac0a2bc75dcb94d42c
Patrick <sanpatricio@users.noreply.github.com>
2019-05-30 14:23:04 +0200

    Changed script output to JSON
    Changed output from simple print() statements to JSON formatting.  Later, I will add an option to specify or configure how someone wants their data (json, csv, .... xml?).

--- CeVichE.py
+++ CeVichE.py
@@ -15,7 +15,7 @@
 #         Red Hat website.
 
 import bs4 as bs
-import urllib2, sys, lxml, re
+import urllib2, sys, lxml, re, json
 
 def buildLink(cve):
         nistURL = "https://nvd.nist.gov/vuln/detail/"
@@ -32,6 +32,7 @@ with open(filename) as inputfile:
 
                 # Red Hat's website does not appreciate being scraped by
                 # automatons.  *Poof!*  We are now a Mozilla browser.
+                # TODO: Use their API before you get got. /Omar
                 agentSpoof = {'User-Agent': 'Mozilla/5.0'}
                 sauce = urllib2.Request(fullURL,headers=agentSpoof)
                 
@@ -46,6 +47,7 @@ with open(filename) as inputfile:
                         print ("%s: An HTTP error has occured - %s" % (cve, status_code))
                 except urllib2.URLError, e:
                         # Something is wrong with the URL
+                        # TODO: e.code doesn't exist.  Returned no useful info when tripped.
                         status_code = e.code
                         print ("%s: A URL error has occured - %s" % (cve, status_code))
                 except (TypeError, AttributeError, KeyError) as e:
@@ -58,9 +60,10 @@ with open(filename) as inputfile:
                         soup  = bs.BeautifulSoup(page,'lxml')
                         theRows = soup.find_all('tr')
 
+                                                # Cycle through all "tr" elements found
                         for row in theRows:
                                 # The following regex seeks only rows labeled "Red Hat Enterprise Linux 6" or "7".
-                                # An end-of-line of $ was used to prevent matching on RHEL "7.3 Telco Extended Update..."
+                                # An end-of-line $ was used to prevent matching on RHEL "7.3 Telco Extended Update..."
                                 tech = row.find('th', text=re.compile(r"Red Hat Enterprise Linux [67]$"))
 
                                 if 'None' in str(tech):
@@ -70,5 +73,14 @@ with open(filename) as inputfile:
                                         package = row.find('td', attrs={'headers':'th-package'}).string
                                         link = ""
                                         if state == "Affected":
-                                                link = "- " + buildLink(cve)
-                                        print ("[%s] %s %s package: %s %s" % (cve, tech.string, package, state, link))
+                                                link = buildLink(cve)
+                                        #print ("[%s] %s %s package: %s %s" % (cve, tech.string, package, state, link))
+                                        rawOutput = {
+                                                "cve": cve,
+                                                "tech": tech.string,
+                                                "package": package,
+                                                "state": state,
+                                                "link": link
+                                        }
+                                output = json.dumps(rawOutput, indent=4, sort_keys=True)
+                        print(output)
